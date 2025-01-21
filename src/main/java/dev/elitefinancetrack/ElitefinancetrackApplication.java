ba6d594ccf81cdb2c9a1eab3b7a514f2679d74a6
V-i-t-h-u-n <70767367+V-i-t-h-u-n@users.noreply.github.com>
2025-01-21 17:27:21 +0530

    Starter spring-boot gradle project

---
+++ src/main/java/dev/elitefinancetrack/ElitefinancetrackApplication.java
@@ -0,0 +1,13 @@
+package dev.elitefinancetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElitefinancetrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElitefinancetrackApplication.class, args);
+	}
+
+}
