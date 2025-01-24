86aa4dacb30ef671308a1795b6d70d02b3c8e727
yogaasrit <yogaasri.t@gmail.com>
2025-01-24 15:26:10 +0530

    Test

--- src/main/java/dev/elitefinancetrack/config/SecurityConfig.java
+++ src/main/java/dev/elitefinancetrack/config/SecurityConfig.java
@@ -1,47 +1,47 @@
-package dev.elitefinancetrack.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User.withUsername("yogaasri")
-                .password("{noop}yog12")
-                .roles("Developer")
-                .build();
-
-        UserDetails user2 = User.withUsername("Alexa")
-                .password("{noop}alexa12")
-                .roles("Admin")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.POST,"api/userName/").hasRole("Developer")
-                        .requestMatchers(HttpMethod.POST,"api/userName/").hasRole("Admin"));
-
-        // Http basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // Disable csrf for stateless rest API
-        http.csrf(AbstractHttpConfigurer::disable);
-        return http.build();
-    }
-}
+//package dev.elitefinancetrack.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user1 = User.withUsername("yogaasri")
+//                .password("{noop}yog12")
+//                .roles("Developer")
+//                .build();
+//
+//        UserDetails user2 = User.withUsername("Alexa")
+//                .password("{noop}alexa12")
+//                .roles("Admin")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                        .requestMatchers(HttpMethod.POST,"api/userName/").hasRole("Developer")
+//                        .requestMatchers(HttpMethod.POST,"api/userName/").hasRole("Admin"));
+//
+//        // Http basic authentication
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        // Disable csrf for stateless rest API
+//        http.csrf(AbstractHttpConfigurer::disable);
+//        return http.build();
+//    }
+//}
